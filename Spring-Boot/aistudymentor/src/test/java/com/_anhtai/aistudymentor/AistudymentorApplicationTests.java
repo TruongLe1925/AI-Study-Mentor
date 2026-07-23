193d1e55572d3215adbf504ca653f0c172f78b24
TruongLe1925 <lephuoctruongppp@gmail.com>
2026-07-23 16:00:07 +0700

    ini

---
+++ Spring-Boot/aistudymentor/src/test/java/com/_anhtai/aistudymentor/AistudymentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com._anhtai.aistudymentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AistudymentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
